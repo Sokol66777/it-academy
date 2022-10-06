8591f2dab789ff557d8ec81fbc7687920f76d395
Sokol66777 <Sokol66777@gmail.com>
2022-10-06 16:17:37 +0300

    task-1

--- jarModule/src/main/java/validation/ValidationPassword.java
+++ jarModule/src/main/java/validation/ValidationPassword.java
@@ -2,12 +2,8 @@ package validation;
 
 public class ValidationPassword {
 
-    public static boolean validationPassword(String password){
+    public static boolean validationPassword(String password) {
         boolean result;
-        result=password.matches("(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}");
-        if(result) {
-            return true;
-        }
-        return false;
+        return password.matches("(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}");
     }
 }
