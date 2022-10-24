09074e4370d07ad217a49a8ab094824aadb1f5ea
Sokol66777 <Sokol66777@gmail.com>
2022-10-24 15:02:02 +0300

    task-4

--- jarModule/src/main/java/dao/UserDAO.java
+++ jarModule/src/main/java/dao/UserDAO.java
@@ -1,10 +1,8 @@
 package dao;
 
-import exceptions.ConnectionToDataBaseException;
+
 import model.User;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
